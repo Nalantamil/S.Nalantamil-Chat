08b1c182c7b5d494867944dab25ae534bd998940
Nalantamil <tamilsundhar49@gmail.com>
2026-07-19 16:24:45 +0530

    Fix eslint alt attribute error

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -823,7 +823,7 @@ function Chat({ username, onLogout }) {
                               {msg.text?.startsWith('__IMAGE__') ? (
                                 <img
                                   src={msg.text.replace('__IMAGE__', '')}
-                                  alt="sent image"
+                                  alt="photo"
                                   className="msg-image"
                                   onClick={() => window.open(msg.text.replace('__IMAGE__', ''), '_blank')}
                                 />
